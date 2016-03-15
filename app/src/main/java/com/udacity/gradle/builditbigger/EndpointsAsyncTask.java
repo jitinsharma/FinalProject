c3f148bb51614ffef60bb6c22ec7bbe0d02feae9
Jitin <jitins2892@gmail.com>
2016-03-15 11:01:54 +0530

    initial commit

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,43 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v4.util.Pair;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.sharma.jitin.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+/**
+ * Created by jsharma on 15-Mar-16.
+ */
+public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://build-it-bigger-1251.appspot.com/_ah/api/");
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+    }
+}
